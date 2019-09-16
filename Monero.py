7e4b8d16e1533e280bf34ec041230df8530f5eab
D34D_FR13ND5 <47918260+ptfn@users.noreply.github.com>
2019-09-16 20:10:07 +0700

    update

--- Monero.py
+++ Monero.py
@@ -1,10 +1,14 @@
 import requests
 import time
-r = requests.get('https://api.cryptonator.com/api/ticker/xmr-usd')
-data = r.json()
-price = data ['ticker']['price']
-change = data ['ticker']['change']
-while True:
-  print("XMR = ",price,"USD")
-  print("Change = ",change)
-  time.sleep(60)
+crypto_wallet = input('Crypto_wallet:')
+if crypto_wallet != '':
+  r = requests.get('https://api.cryptonator.com/api/ticker/' + crypto_wallet + '-usd')
+  data = r.json()
+  price = data ['ticker']['price']
+  change = data ['ticker']['change']
+  while True:
+    print(crypto_wallet,'=',price,"USD")
+    print("change = ",change)
+    time.sleep(120)
+else :
+  print("error")
