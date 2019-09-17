569fb9761812e61a449fbab8bd40ee209ad2c62f
D34D_FR13ND5 <47918260+ptfn@users.noreply.github.com>
2019-09-17 21:08:28 +0700

    Update

--- Monero.py
+++ Monero.py
@@ -2,13 +2,17 @@ import requests
 import time
 crypto_wallet = input('Crypto_wallet:')
 if crypto_wallet != '':
-  r = requests.get('https://api.cryptonator.com/api/ticker/' + crypto_wallet + '-usd')
-  data = r.json()
-  price = data ['ticker']['price']
-  change = data ['ticker']['change']
-  while True:
-    print(crypto_wallet,'=',price,"USD")
-    print("change = ",change)
-    time.sleep(120)
+  wallet = input('Wallet:')
+  if wallet != '':
+    r = requests.get('https://api.cryptonator.com/api/ticker/' + crypto_wallet + "-" + wallet)
+    data = r.json()
+    price = data ['ticker']['price']
+    change = data ['ticker']['change']
+    while True:
+      print(crypto_wallet,'=',price,wallet)
+      print("change = ",change)
+      time.sleep(120)
+  else :
+    print('error')
 else :
   print("error")
